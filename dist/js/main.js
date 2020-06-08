942842ecba9127104dac9795f13bff88daef0b3a
Yuri Kuzmichev <2091god@mail.ru>
2020-06-08 17:20:17 +0500

    fix: fixed validate and send form

--- dist/js/main.js
+++ dist/js/main.js
@@ -30,30 +30,96 @@ var modal = $('.modal'),
       }
     });
   });
-  // Валидация формы
-  $( document ).ready(function() {
-    $("#btn").click(
-      function(){
-			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
-			return false; 
-		}
-	);
+
+/* Валидация формы - 2 */
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const text = document.getElementById('text');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
 });
-function sendAjaxForm(result_form, ajax_form, url) {
-    $.ajax({
-        url: url   , //url страницы (action_ajax_form.php)
-        // ('../php/action_ajax_form.php') 
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
-        	result = $.parseJSON(response);
-        	$('#result_form').html('Имя: '+result.first_name +'<br>Email: '+ result.email+'<br>Message: '+ result.message);
-          modal.removeClass('modal--visible');
-          message.toggleClass('message--visible');
-    	},
-    	error: function(response) { // Данные не отправлены
-            $('#result_form').html('Ошибка. Данные не отправлены.');
-    	}
- 	});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const textValue = text.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+    setSuccessFor(username);
+    $('#small1').addClass('not_error');
+    
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+    $('#small2').addClass('not_error');
+	}
+	
+	if(textValue === '') {
+		setErrorFor(text, 'Message cannot be blank');
+	} else {
+		setSuccessFor(text);
+    $('#small3').addClass('not_error');
+	}
 }
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+  $('form#form').submit(function(e){
+
+    // Запрещаем стандартное поведение для кнопки submit
+    e.preventDefault();
+  
+    // После того, как мы нажали кнопку "Отправить", делаем проверку,
+    // если кол-во полей с классом .not_error равно 3 (так как у нас всего 3 поля), то есть все поля заполнены верно,
+    // выполняем наш Ajax сценарий и отправляем письмо адресату
+  
+    if($('.not_error').length == 3) {
+       // Eще одним моментом является то, что в качестве указания данных для передачи обработчику send.php, мы обращаемся $(this) к нашей форме,
+       // и вызываем метод .serialize().
+       // Это очень удобно, т.к. он сразу возвращает сгенерированную строку с именами и значениями выбранных элементов формы.
+        $.ajax({
+          url: 'send.php',
+          type: 'post',
+          data: $(this).serialize(),
+  
+          beforeSend: function(xhr, textStatus){ 
+          },
+  
+          success: function(response){
+            modal.removeClass('modal--visible');
+            $(form)[0].reset();
+            message.toggleClass('message--visible');
+          }
+        }); // end ajax({...})
+        }
+        // Иначе, если количество полей с данным классом не равно значению 3, мы возвращаем false,
+        // останавливая отправку сообщения в невалидной форме
+        else {
+          return false;
+        }
+    
+    }); // end submit()
---
+++ src/send.php
@@ -0,0 +1,43 @@
+<?php
+$name = $_POST['username'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->Charset = 'UTF-8';                      
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'nneqm388@gmail.com';                     // SMTP username
+    $mail->Password   = 'assasin47';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('nneqm388@gmail.com', 'Юрий');
+    $mail->addAddress('gvirtums@mail.ru');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Afrianska';
+    $mail->Body    = "First Name: ${name}. <br>Email: ${email}. <br>Message: ${message}.";
+
+    if ($mail->send()) {
+        echo "ok";
+    } else {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+    
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
